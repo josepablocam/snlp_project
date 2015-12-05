ab371d7da0cb2daf5d24ba77131c62d03c645b72
Jose Cambronero <jose.cambronero@cs.nyu.edu>
2015-12-05 17:00:44 -0500

    [extend] added return to string performance message in models, included Xiuyan's rewriting of model reporting that uses generic wrapper report_model

--- src/python/Models.py
+++ src/python/Models.py
@@ -6,7 +6,8 @@ from sklearn.naive_bayes import GaussianNB
 import time
 from sklearn import svm
 from sklearn.lda import LDA
-import Features
+from sklearn.linear_model.logistic import LogisticRegression
+from sklearn.mixture.gmm import GMM
 
 def report_performance(gold, predicted):
     """
@@ -50,13 +51,13 @@ def report_model(make_model, train_X, train_Y, test_X, test_Y, model_name = None
 def report_BernoulliNB(train_X, train_Y, test_X, test_Y, **args):
     model_name = "Bernoulli Naive Bayes"
     make_model = lambda: BernoulliNB(**args) if args else BernoulliNB(alpha = 0.1)
-    report_model(make_model, train_X, train_Y, test_X, test_Y, model_name)
+    return report_model(make_model, train_X, train_Y, test_X, test_Y, model_name)
 
 def report_GaussianNB(train_X, train_Y, test_X, test_Y):
     model_name = "Gaussian Naive Bayes"
     make_model = lambda: GaussianNB()
     # Gaussian NB wants matrices to be dense
-    report_model(make_model, train_X.todense(), train_Y, test_X.todense(), test_Y, model_name)
+    return report_model(make_model, train_X.todense(), train_Y, test_X.todense(), test_Y, model_name)
 
 # def report_GaussianNB(train_X, train_Y, test_X, test_Y):
 #     # Naive Bayes model
@@ -69,56 +70,40 @@ def report_GaussianNB(train_X, train_Y, test_X, test_Y):
 #     print "Accuracy: %f" % accuracy_score(test_Y, nbClassifications1)
 
 def report_SVM_linearK(train_X, train_Y, test_X, test_Y):
+    model_name = "SVM with Linear Kernel"
+    make_model = lambda: LinearSVC()
+    return report_model(make_model, train_X, train_Y, test_X, test_Y, model_name)
 
-    svmClassifier = LinearSVC()
-    print "start svm training: ", time.asctime()
-    svmClassifier.fit(train_X, train_Y)
-    print "end svm  training: ", time.asctime()
-    svmClassifications = svmClassifier.predict(test_X)
-    print classification_report(test_Y, svmClassifications)
-    print "Accuracy: %f" % accuracy_score(test_Y, svmClassifications)
 
 def report_SVM_rbfK(train_X, train_Y, test_X, test_Y):
-
-    svmClassifier = svm.SVC(C=1, kernel='rbf')
-    print "start svm training: ", time.asctime()
-    svmClassifier.fit(train_X, train_Y)
-    print "end svm  training: ", time.asctime()
-    svmClassifications = svmClassifier.predict(test_X)
-    print classification_report(test_Y, svmClassifications)
-    print "Accuracy: %f" % accuracy_score(test_Y, svmClassifications)
+    model_name = "SVM with RBF Kernel"
+    make_model = lambda: svm.SVC(C=1, kernel='rbf')
+    return report_model(make_model, train_X, train_Y, test_X, test_Y, model_name)
 
 def report_SVM_sigK(train_X, train_Y, test_X, test_Y):
+    model_name = "SVM with Sigmoid Kernel"
+    make_model = lambda: svm.SVC(C=1, kernel='sigmoid')
+    return report_model(make_model, train_X, train_Y, test_X, test_Y, model_name)
 
-    svmClassifier = svm.SVC(C=1, kernel='sigmoid')
-    print "start svm training: ", time.asctime()
-    svmClassifier.fit(train_X, train_Y)
-    print "end svm  training: ", time.asctime()
-    svmClassifications = svmClassifier.predict(test_X)
-    print classification_report(test_Y, svmClassifications)
-    print "Accuracy: %f" % accuracy_score(test_Y, svmClassifications)
 
 def report_SVM_polyK(train_X, train_Y, test_X, test_Y):
+    model_name = "SVM with Polynomial Kernel"
+    make_model = lambda: svm.SVC(C=1, kernel='poly')
+    return report_model(make_model, train_X, train_Y, test_X, test_Y, model_name)
 
-    svmClassifier = svm.SVC(C=1, kernel='poly')
-    print "start svm training: ", time.asctime()
-    svmClassifier.fit(train_X, train_Y)
-    print "end svm  training: ", time.asctime()
-    svmClassifications = svmClassifier.predict(test_X)
-    print classification_report(test_Y, svmClassifications)
-    print "Accuracy: %f" % accuracy_score(test_Y, svmClassifications)
 
 def report_FisherLDA(train_X, train_Y, test_X, test_Y):
-    fisherClassifier = LDA()
-    print "start lda training: ", time.asctime()
-    fisherClassifier.fit(train_X, train_Y)
-    print "end lda  training: ", time.asctime()
-    fisherClassifications = fisherClassifier.predict(test_X)
-    print classification_report(test_Y, fisherClassifications)
-    print "Accuracy: %f" % accuracy_score(test_Y, fisherClassifications)
+    model_name = "Fisher Linear Classifier"
+    make_model = lambda: LDA()
+    return report_model(make_model, train_X, train_Y, test_X, test_Y, model_name)
+
 
 def report_GMM(train_X, train_Y, test_X, test_Y):
-    print "Haven't implemented"
+    model_name = "Multivariate Gaussian Classifier"
+    make_model = lambda: GMM()
+    return report_model(make_model, train_X, train_Y, test_X, test_Y, model_name)
 
 def report_LogisticReg(train_X, train_Y, test_X, test_Y):
-    print "Haven't implemented"
+    model_name = "Logistic Regression Classifier"
+    make_model = lambda: LogisticRegression()
+    return report_model(make_model, train_X, train_Y, test_X, test_Y, model_name)
